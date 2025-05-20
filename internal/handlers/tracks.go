eea8a019614f82bfe42e13bca762b4a90aade05c
3duu <eduu.porto@gmail.com>
2025-05-19 23:47:44 -0300

    playing tracks

--- internal/handlers/tracks.go
+++ internal/handlers/tracks.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
-	"log"
 	"net/http"
 	"path/filepath"
 	"spotify-mock-api/internal/models"
@@ -20,8 +19,17 @@ func NewTrackHandler(db *gorm.DB) *TrackHandler {
 // getTrackByID retrieves song metadata and returns it along with an audio URL
 func (h *TrackHandler) GetTrackByID(c *gin.Context) {
 	var song models.Song // make sure Song is imported or duplicated here
-	if err := h.DB.First(&song, "id = ?", c.Param("id")).Error; err != nil {
+	/*if err := h.DB.First(&song, "id = ?", c.Param("id")).Error; err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "song not found"})
+		return
+	}*/
+	id := c.Param("id")
+	if err := h.DB.
+		Preload("Artist").
+		Preload("Album").
+		First(&song, "id = ?", id).
+		Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "not found"})
 		return
 	}
 
@@ -46,41 +54,34 @@ func (h *TrackHandler) GetTrackByID(c *gin.Context) {
 
 // getTrackAudio streams the MP3 file (always the same file)
 func GetTrackAudio(c *gin.Context) {
-	log.Print("Get Audio")
 	c.File(filepath.Join("media", "song.mp3"))
 }
 
 func GetArtistByID(c *gin.Context) {
-	log.Print("Get Artist")
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{"id": id, "name": "Mock Artist"})
 }
 
 func GetAlbumByID(c *gin.Context) {
-	log.Print("Get Album")
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{"id": id, "title": "Mock Album"})
 }
 
 func GetPlaylistByID(c *gin.Context) {
-	log.Print("Get Playlist")
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{"id": id, "name": "Mock Playlist"})
 }
 
 func Search(c *gin.Context) {
-	log.Print("Search")
 	query := c.Query("q")
 	c.JSON(http.StatusOK, gin.H{"query": query, "results": []string{}})
 }
 
 func GetProfile(c *gin.Context) {
-	log.Println("Get Profile")
 	c.JSON(http.StatusOK, gin.H{"id": "user123", "display_name": "Mock User"})
 }
 
 func Login(c *gin.Context) {
-	log.Print("Login User")
 	// TODO: authenticate and return mock token
 	c.JSON(http.StatusOK, gin.H{"access_token": "mock_token"})
 }
